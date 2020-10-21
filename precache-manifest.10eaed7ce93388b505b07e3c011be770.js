2519ed33998e45d3a0038ac2dace815840dd3153
TishchenkoSergey <tishchenko.seriy@gmail.com>
2020-10-21 15:33:30 +0300

    Updates

---
+++ precache-manifest.10eaed7ce93388b505b07e3c011be770.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5136144ae730c293d1a84a02f082d737",
+    "url": "./index.html"
+  },
+  {
+    "revision": "08cb0991388dcba03040",
+    "url": "./static/css/main.4e0e8fc7.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08cb0991388dcba03040",
+    "url": "./static/js/main.3ca59db1.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
